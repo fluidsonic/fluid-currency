642f6b1e164c124bdeae41a1d6b7f8885aca8175
Marc Knaup <marc@knaup.io>
2020-12-10 16:57:08 +0000

    🎉 0.9.3

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Installation
 
 ```kotlin
 dependencies {
-    implementation("io.fluidsonic.currency:fluid-currency:0.9.2")
+	implementation("io.fluidsonic.currency:fluid-currency:0.9.3")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.14"
 }
 
-fluidLibrary(name = "currency", version = "0.9.2")
+fluidLibrary(name = "currency", version = "0.9.3")
 
 fluidLibraryModule(description = "Kotlin multiplatform currency library") {
 	targets {
